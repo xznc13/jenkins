e87ce5b2d8f28f9d493f2544abcec27441c9b58f
xznc13 <stephen.butcher@outlook.com>
2016-02-18 19:27:49 +0000

    Initial build files

---
+++ files/setenv.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+export JENKINS_HOME="/jenkins"
+export CATALINA_HOME="/usr/share/apache-tomcat-8.0.29"
+export JAVA_HOME="/usr/java/jdk1.8.0_65"
+export JRE_HOME="/usr/java/jdk1.8.0_65"
+export JAVA_OPTS="$JAVA_OPTS -Xms384m -Xmx768m"
+echo $JAVA_OPTS
+
